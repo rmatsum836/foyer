733c8c59d8872c300c8b60f84023568cc53a0084
Ray Matsumoto <raymatsumoto@Macintosh-2.local>
2017-05-15 13:31:02 -0500

    Changes to forcefields

---
+++ foyer/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.3.0'
+short_version = '0.3.0'
+full_version = '0.3.0+4d2eb006efbc9b788ecd642e197d01dd7e0e7f1b'
+git_revision = '4d2eb006efbc9b788ecd642e197d01dd7e0e7f1b'
+release = False
